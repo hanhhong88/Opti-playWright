3d7577474e38e7b2436ef4daa40f39648f496d88
Hanh Nguyen <hanh.nguyen.thi.hong@ericson.com>
2024-06-20 11:33:10 +0700

    Add test case Download

---
+++ tests/Download-PDF file.spec.ts
@@ -0,0 +1,37 @@
+import {expect, test } from '@playwright/test';
+test('Download pdf file', async ({ page , headless}) => {
+
+  await page.goto('https://www.ericsson.com/en');
+    console.log(headless);
+
+    await page.locator("a#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll").click();
+    await page.locator("a[title = 'Link to pdf document']").scrollIntoViewIfNeeded();
+
+
+  if(headless){
+    const [download, click]= await Promise.all([
+      page.waitForEvent('download'),
+      page.locator("a[title = 'Link to pdf document']").click()
+    ]
+
+    )
+    // const downloadPromise = page.waitForEvent('download')
+    // await page.locator("a[title = 'Link to pdf document']").click();
+    // const download = await downloadPromise;
+    // console.log(download.suggestedFilename());
+    expect(download.suggestedFilename()).toEqual("modern-slavery-and-human-trafficking-statement.pdf");
+  }
+
+  else {
+    const [newPage] = await Promise.all([
+      page.waitForEvent('load'),
+      page.locator("a[title = 'Link to pdf document']").click()
+    ])
+
+    newPage.on('response', async (response) => {
+      expect(response.headers()['content-type'].tobe('application/pdfs'))
+    })
+
+    await newPage.reload();
+  }
+  });
